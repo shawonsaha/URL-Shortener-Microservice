b4eb153d294c56b1b584a2b8333ad08a12b7a2eb
Shawon Saha <shawon.saha@outlook.com>
2020-06-04 02:16:28 +0600

    Hashing and routing works properly

--- server.js
+++ server.js
@@ -1,69 +1,65 @@
-var express = require("express");
-var mongo = require("mongodb");
-var mongoose = require("mongoose");
-var url = require("url");
-const shortid = require("shortid");
+var express = require("express"); // minimal and flexible Node.js web application framework
+const dotenv = require("dotenv").config(); // allows you to separate secrets from your source code
+var mongoose = require("mongoose"); // schema-based solution to model application data
+var url = require("url"); // url validation checker
+var sh = require("shorthash"); //short hash string generator
 
-// Connect to MongoDB
-mongoose.connect(
-  "mongodb+srv://shawonsaha:5DnUZBV4nHr3z8pw@nodefcc-wxnma.mongodb.net/test?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-var urlSchema = new mongoose.Schema({
-  url: String,
-  hash: {
-    type: String,
-    default: shortid.generate,
-  },
+// Connection to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-var Url = mongoose.model("Url", urlSchema);
+// Mongoose Schema
+var urlSchema = new mongoose.Schema({
+  url: String,
+  hash: String,
+});
 
-var PORT = 3000;
+// Mongoose Model
+var Url = mongoose.model("Url", urlSchema);
 
 // Initialize Express
 var app = express();
 
-// Parse request body as JSON
+// parses incoming requests (request.body) with urlencoded payloads and is based on body-parser
 app.use(express.urlencoded({ extended: true }));
+// parses incoming requests (request.body) with JSON payloads and is based on body-parser
 app.use(express.json());
 
-// Make public static folder
+// Make public static folder accesible
 app.use("/public", express.static(process.cwd() + "/public"));
 
-// Routes
+// Home Page
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
+// api returns {original_url, short_route}
 app.post("/api/shorturl/new", function (req, res) {
-  // res.json({ req: req.body.url });
-
-  let result = url.parse(req.body.url);
-  if (result.hostname) {
-    var myData = new Url(req.body);
+  let userInput = url.parse(req.body.url); // parsing and storing user input
+  // if it is a valid url
+  if (userInput.hostname) {
+    let short_url = sh.unique(req.body.url);
+    var myData = new Url({ url: req.body.url, hash: short_url }); // API that send short route for valid url in json format
     myData
       .save()
       .then((item) => {
-        res.send("item saved to database.");
+        res.json({ original_url: userInput.href, short_url }); // API which user can grab
       })
       .catch((err) => {
         res.status(400).send("unable to save to database");
       });
   } else {
-    res.send("Invalid URL");
+    res.json({ error: "invalid URL" });
   }
 });
 
+// action while client hit to the short url
 app.get("/:hash", function (req, res) {
-  // console.log(req.params.hash);
-  Url.findOne({ hash: req.params.hash }, function (err, doc) {
-    if (doc) {
-      res.redirect(doc.url);
+  Url.findOne({ hash: req.params.hash }, function (err, route) {
+    if (route) {
+      res.redirect(route.url);
     } else {
       res.redirect("/");
     }
@@ -71,6 +67,6 @@ app.get("/:hash", function (req, res) {
 });
 
 // Start the server
-app.listen(PORT, function () {
-  console.log("Listening on port " + PORT + ".");
+app.listen(3000, function () {
+  console.log("Listening on port 3000");
 });
